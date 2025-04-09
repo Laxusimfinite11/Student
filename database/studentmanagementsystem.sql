33834b33b9055b07851b95d34f63a16cad2e2eb7
Laxusimfinite11 <laxustaladro@gmail.com>
2025-04-09 09:37:30 +0800

    Update Database Content

--- database/studentmanagementsystem.sql
+++ database/studentmanagementsystem.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Apr 03, 2025 at 01:39 PM
+-- Host: localhost:3307
+-- Generation Time: Apr 09, 2025 at 03:35 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,102 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `studentmanagementsystem`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grades`
---
-
-CREATE TABLE `grades` (
-  `grades_id` int(5) NOT NULL,
-  `subject_id` int(5) NOT NULL,
-  `user_id` int(5) NOT NULL,
-  `grades` decimal(3,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `grades`
---
-
-INSERT INTO `grades` (`grades_id`, `subject_id`, `user_id`, `grades`) VALUES
-(5, 4, 3, 2.50),
-(6, 3, 4, 5.00),
-(8, 5, 5, 1.25),
-(20, 3, 3, 1.50),
-(21, 7, 3, 1.00),
-(25, 4, 6, 1.25),
-(28, 5, 24, 1.25),
-(29, 4, 24, 1.50),
-(30, 13, 24, 1.75),
-(31, 7, 24, 1.25),
-(32, 3, 26, 4.00),
-(33, 13, 26, 2.00),
-(34, 9, 26, 1.25),
-(35, 7, 26, 1.25),
-(36, 5, 26, 3.00),
-(37, 18, 26, 5.00),
-(38, 22, 26, 0.00),
-(39, 4, 17, 1.00),
-(41, 14, 3, 5.00),
-(42, 2, 3, 1.25);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `otp`
---
-
-CREATE TABLE `otp` (
-  `otp_id` int(5) NOT NULL,
-  `user_id` int(5) NOT NULL,
-  `otp_code` varchar(6) NOT NULL,
-  `expiry_time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subject`
---
-
-CREATE TABLE `subject` (
-  `subjectID` int(5) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `description` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `subject`
---
-
-INSERT INTO `subject` (`subjectID`, `name`, `description`) VALUES
-(1, 'Intensive English Review', 'Intensive English Review is improving English skills.'),
-(2, 'Discrete 1', 'Discrete 1 is a math course focusing on discrete structures like sets, graphs, and logic.'),
-(3, 'Math Elective', 'Math Elective is a specialized math course.'),
-(4, 'Computer Programming', 'Computer programming is creating software.'),
-(5, 'CS Elective 1', 'CS Elective 1 is a specialized computer science course.'),
-(7, 'Object Oriented Programming (OOP)', 'OOP is a programming paradigm that organizes software design around objects, which combine data and behavior.'),
-(8, 'Algorithm Complexity', 'Algorithm complexity measures how efficient an algorithm is.'),
-(9, 'Intro to Computing', 'Intro to computing is learning about computers.'),
-(13, 'Application Development', 'Application development is creating software apps.'),
-(14, 'Palawan Study', 'Palawan Studies explores the history, culture, and environment of Palawan, Philippines.'),
-(15, 'Data Structure and Algorithms', 'Data Structures and Algorithms is about organizing data and creating efficient algorithms to solve problems.'),
-(16, 'Networks and Communications', 'Networks and Communications is about connecting devices.'),
-(17, 'Computational Science', 'Computational science uses computers to solve complex problems.'),
-(18, 'Fundamentals in Electronics', 'Fundamentals in Electronics is about understanding basic electronic circuits and components.'),
-(19, 'Architecture and Organizations', 'Architecture and Organizations is about system design.'),
-(20, 'Information Management', 'Information management is about organizing and using information effectively.'),
-(21, 'Advances Database Systems', 'Advanced Database Systems is about studying complex database systems and their advanced features.'),
-(22, 'Programming Languages', 'Programming languages are tools for creating software.'),
-(23, 'Software Engineering 1', 'Software Engineering 1 is about building software systematically.'),
-(24, 'Software Engineering 2', 'Software Engineering 2 is about advanced software development practices.'),
-(25, 'Information Assurance and Security', 'Information Assurance and Security is about protecting information and systems from threats.'),
-(26, 'Digital Image Processing', 'Digital Image Processing is about manipulating digital images.'),
-(27, 'Human Computer Interaction', 'HCI is about designing user-friendly interfaces.'),
-(28, 'Proposal Writing', 'Proposal writing is creating a document to propose an idea or plan for a thesis.'),
-(29, 'Web Systems and Technologies', 'Web Systems and Technologies is about building websites and web applications.'),
-(30, 'Practicum', 'Practicum is hands-on learning experience.');
 
 -- --------------------------------------------------------
 
@@ -142,7 +46,6 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `mobile_number`, `email`, `password`, `role`, `otp`, `otp_expiry`, `is_verified`) VALUES
 (3, 'Erroll', 'Montalvo', '09669727834', 'Erroll@gmail.com', '$2y$10$sLnhbjmzCU.AUaGd8V1DCO64XaTpw0Fwgk4D5iTCuHcNRUQDF.K/i', 'Student', NULL, NULL, 0),
-(4, 'Ronald', 'Palo', '09669447833', 'Nald@yahoo.com', '$2y$10$.njzdnP0KX/S7dfJ36wtle6wVBfod.GVuFZw31cgDTveKu9S4Nna.', 'Student', NULL, NULL, 0),
 (5, 'Juan Carlos', 'Gonzalez', '09669837839', 'JuanCarlos@yahoo.com', '$2y$10$Tvw4FDNznNdlFTaRwuyTouOssa2GRWEpQAAHezZJeIntj9TNsc1xK', 'Student', NULL, NULL, 0),
 (6, 'Patrick John', 'Maigue', '09479727839', 'patrick@gmail.com', '$2y$10$B5t5XoIZ2M7qYaqrgGFp4.XHfg4G3LZ4Qe38yYglNk2L/O9i9hF2q', 'Student', NULL, NULL, 0),
 (10, 'Man', 'Sok', '1212123', 'www@gmail.com', 'dq2q2dacadsd', '', NULL, NULL, 0),
@@ -152,34 +55,13 @@ INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `mobile_number`, `ema
 (24, 'Lexical', 'Analysis', '12312312312', 'lexical@yahoo.com', '$2y$10$IIw2JWEtZBQtXHA0rKcfyu/xXaoURLAMl5pPYjnsr6BzYF3e9IqtS', 'Student', NULL, NULL, 0),
 (25, 'tester', '123', '09129025593', 'tester@gmail.com', '$2y$10$wdAVImM/oEJHOPmrCyLKWOVLQ.sbMx1eG2gspP0y8ZM8cxVTYvzYq', 'Admin', NULL, NULL, 0),
 (26, 'lexus', 'taladro', '9879025591', 'lexus@yahoo.com', '$2y$10$OM5avtH43AKTWcefFzmTseZAdgqd8P7V.qP7SbDkSGWrJmBOquU2u', 'Student', NULL, NULL, 0),
-(27, 'College of Science', 'Office', '63917123456', 'CS@palawan.com', '$2y$10$5sVfXkpvRIg8BPaEv4uqHOWJnouhzowkhkiD2mzNaVbYx7nKD/AZy', 'Admin', NULL, NULL, 0),
 (29, 'admin', 'admin', '1234566789', 'admin@gmail.com', '$2y$10$RISiCHqgqj4h/yMeDWlWbO8Uymto6/HeO1xnVuPTahK/4jAXsd92q', 'Admin', '280478', '2025-04-03 11:26:10', 0),
-(30, 'Mar', 'Mar', '978564545', 'mar@gmail.com', '$2y$10$WTqOmz53MCt0lNIVzxWVLOq0es6mYsgzqj6RZxs82ok3XnOdtlPmK', 'Student', NULL, NULL, 0);
+(31, 'Lexus', 'Taladro', '09669727839', 'laxustaladro@gmail.com', '$2y$10$VOZh4RHU3hH5bg4u1KxPE.lq7qqZ75eiNJf8kz769xk2sZxRCmPOW', 'Admin', NULL, NULL, 0),
+(32, 'Jan Ronald', 'Palo', '9129025591', 'ronaldkun999@gmail.com', '$2y$10$N5sp2bVg2C7Ws7jm8RKcX.PuBJ7ZzVKEb4hB3L152SI9Qnt0sIlJ.', 'Student', NULL, NULL, 0);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `grades`
---
-ALTER TABLE `grades`
-  ADD PRIMARY KEY (`grades_id`),
-  ADD KEY `subject_id` (`subject_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `otp`
---
-ALTER TABLE `otp`
-  ADD PRIMARY KEY (`otp_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `subject`
---
-ALTER TABLE `subject`
-  ADD PRIMARY KEY (`subjectID`);
 
 --
 -- Indexes for table `users`
@@ -194,45 +76,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `grades`
---
-ALTER TABLE `grades`
-  MODIFY `grades_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
-
---
--- AUTO_INCREMENT for table `otp`
---
-ALTER TABLE `otp`
-  MODIFY `otp_id` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `subject`
---
-ALTER TABLE `subject`
-  MODIFY `subjectID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `grades`
---
-ALTER TABLE `grades`
-  ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subjectID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Constraints for table `otp`
---
-ALTER TABLE `otp`
-  ADD CONSTRAINT `otp_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
